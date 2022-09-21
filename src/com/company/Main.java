f7b9de549b3988dd2a4783fc00eb137f6cef5dd5
Mamedov08 <hanchikmamedov87@gmail.com>
2022-09-21 23:40:15 +0600

    ewfr

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,15 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(funkc(generateRandomAge(),18));
-        System.out.println(funkc(generateRandomAge(),20));
-        System.out.println(funkc(generateRandomAge(),40));
-        System.out.println(funkc(generateRandomAge(),-10));
-        System.out.println(funkc(generateRandomAge(),32));
+        System.out.println(funkc(generateRandomAge(), 18));
+        System.out.println(funkc(generateRandomAge(), 20));
+        System.out.println(funkc(generateRandomAge(), 40));
+        System.out.println(funkc(generateRandomAge(), -10));
+        System.out.println(funkc(generateRandomAge(), 32));
+
     }
 
-    public static String funkc (int age , int temp ) {
+    public static String funkc(int age, int temp) {
         if (age > 20 && age < 45 && temp > -20 && temp < 30) {
             return "Можно идти гулять";
         } else if (age < 20 && temp > 0 && temp < 28) {
@@ -24,9 +25,10 @@ public class Main {
 
         }
     }
-       public static int generateRandomAge() {
-           Random random = new Random();
-           int a = random.nextInt(100);
-           return a;
-       }
+
+    public static int generateRandomAge() {
+        Random random = new Random();
+        int a = random.nextInt(100);
+        return a;
+    }
 }
